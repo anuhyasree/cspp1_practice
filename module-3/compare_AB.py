7d527a8bae3dce083f09aca1e52272a565890725
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-01 19:38:45 +0530

    anu

---
+++ module-3/compare_AB.py
@@ -0,0 +1,19 @@
+"""comparision of two variables"""
+def compare_ref(parameter):
+#Definding the function for the type
+    return type(parameter)
+# VARA = int(input ("Enter the first variable value"))
+# VARB = int(input("Enter the second variable value"))
+
+VARA = "12"
+VARB = 12
+
+STRING_TYPE = "string"
+if compare_ref(VARA) == compare_ref(STRING_TYPE)  or compare_ref(VARB) == compare_ref(STRING_TYPE):
+    print("String Involved")
+elif VARA > VARB:
+    print("Bigger")
+elif VARA == VARB:
+    print("Equal")
+else:
+    print("Smaller")
